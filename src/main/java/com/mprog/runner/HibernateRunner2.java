60d516cf582a0527dbc513fe2b4a990394b8af77
Munir Zikrullaev <munirprog@gmail.com>
2021-12-06 20:28:57 +0300

    add logging

--- src/main/java/com/mprog/runner/HibernateRunner2.java
+++ src/main/java/com/mprog/runner/HibernateRunner2.java
@@ -4,11 +4,17 @@ import com.mprog.converter.BirthdayConverter;
 import com.mprog.entity.User;
 import com.mprog.util.HibernateUtil;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@Slf4j
 public class HibernateRunner2 {
 
+//    private static final Logger log = LoggerFactory.getLogger(HibernateRunner2.class);
     public static void main(String[] args) {
 
         var user = User.builder()
@@ -17,24 +23,31 @@ public class HibernateRunner2 {
                 .lastname("Ivanov")
                 .build();
 
+        log.info("User entity is in transient state, object: {}", user);
         try (var sessionFactory = HibernateUtil.buildSessionFactory()){
-            try (var session1 = sessionFactory.openSession()) {
-                session1.beginTransaction();
+            var session1 = sessionFactory.openSession();
+            try (session1) {
+                var transaction = session1.beginTransaction();
+                log.trace("Transaction  is begun, {}", transaction);
 
                 session1.saveOrUpdate(user);
-
+                log.trace("User is in persistent state: {}, session {}", user, session1);
                 session1.getTransaction().commit();
             }
 
-            try (var session2 = sessionFactory.openSession()) {
-                session2.beginTransaction();
-
-                user.setLastname("Petrov");
-                //                session2.delete(user);
+            log.warn("User is in detached state: {}, session: {}", user, session1);
+//            try (var session2 = sessionFactory.openSession()) {
+//                session2.beginTransaction();
+//
+//                user.setLastname("Petrov");
+//                //                session2.delete(user);
 //                session2.refresh(user);
-                var merge = session2.merge(user);
-                session2.getTransaction().commit();
-            }
+//                var merge = session2.merge(user);
+//                session2.getTransaction().commit();
+//            }
+        } catch (Exception exception) {
+            log.error("Exception occurred", exception);
+            throw exception;
         }
     }
 }
