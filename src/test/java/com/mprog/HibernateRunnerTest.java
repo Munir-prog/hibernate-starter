53e937a51ca8fbea985e9b4a9be5da27e4f63400
Munir Zikrullaev <munirprog@gmail.com>
2021-12-14 18:15:45 +0300

    cascade types with collections

--- src/test/java/com/mprog/HibernateRunnerTest.java
+++ src/test/java/com/mprog/HibernateRunnerTest.java
@@ -28,6 +28,39 @@ import static org.junit.jupiter.api.Assertions.*;
 class HibernateRunnerTest {
 
     @Test
+    void deleteCompany() {
+        @Cleanup var sessionFactory = HibernateUtil.buildSessionFactory();
+        @Cleanup Session session = sessionFactory.openSession();
+
+        session.beginTransaction();
+
+        var company = session.get(Company.class, 3);
+        session.delete(company);
+
+        session.getTransaction().commit();
+    }
+
+
+    @Test
+    void addUserToNewCompany() {
+        @Cleanup var sessionFactory = HibernateUtil.buildSessionFactory();
+        @Cleanup Session session = sessionFactory.openSession();
+
+        session.beginTransaction();
+
+        var company = Company.builder()
+                .name("Facebook")
+                .build();
+
+        var sveta = User.builder().username("sveta@gmail.com").build();
+        company.addUser(sveta);
+
+        session.save(company);
+
+        session.getTransaction().commit();
+    }
+
+    @Test
     void oneToMany() {
         @Cleanup var sessionFactory = HibernateUtil.buildSessionFactory();
         @Cleanup Session session = sessionFactory.openSession();
